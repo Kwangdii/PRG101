b6ee70adfa161e15606e5734d128752152ae3bfc
unknown <08250269.sce@rub.edu.bt>
2026-04-02 16:33:20 +0530

    tuple

---
+++ tupleEx.py
@@ -0,0 +1,21 @@
+my_tuple=('Hello', 123456)
+print(type(my_tuple))
+print(my_tuple)
+
+print(my_tuple[1]) # Accessing the last element of the tuple (index starts at 0)
+
+a, b = my_tuple # Unpacking the tuple into separate variables
+print(b)
+
+new_tuple = tuple(a) # Converting a string to a tuple
+print(new_tuple)
+
+concatenated_tuple = my_tuple + new_tuple # Concatenating two tuples
+print(concatenated_tuple)
+
+print(concatenated_tuple[2:6:2]) # Slicing the tuple to get every second element from index 2 to 5
+
+print(concatenated_tuple[::-1]) # Reversing the tuple using slicing
+
+del my_tuple
+print(my_tuple) # This will raise an error because my_tuple has been deleted
