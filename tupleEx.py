ab239e05b596b5bfb6075fb21e999d3d4dbf372a
unknown <08250269.sce@rub.edu.bt>
2026-04-03 15:10:18 +0530

    tuple

--- tupleEx.py
+++ tupleEx.py
@@ -13,9 +13,11 @@ print(new_tuple)
 concatenated_tuple = my_tuple + new_tuple # Concatenating two tuples
 print(concatenated_tuple)
 
+print(concatenated_tuple[6:1:-4]) # Slicing the tuple to get every fourth element from index 2 to 6 and then reversing the result
+
 print(concatenated_tuple[2:6:2]) # Slicing the tuple to get every second element from index 2 to 5
 
-print(concatenated_tuple[::-1]) # Reversing the tuple using slicing
+print(concatenated_tuple [1:6:-1])
 
-del my_tuple
-print(my_tuple) # This will raise an error because my_tuple has been deleted
+'''del my_tuple
+print(my_tuple) # This will raise an error because my_tuple has been deleted'''
