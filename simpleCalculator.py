905d1d5e27bb41745bf6c7bb96b1572aef1f872b
unknown <08250269.sce@rub.edu.bt>
2026-04-28 11:39:48 +0530

    calculator trial

---
+++ simpleCalculator.py
@@ -0,0 +1,34 @@
+def Add(a,b):
+    return(a+b)
+
+def Substract(a,b):
+    return(a-b)
+
+def Multiply(a,b):
+    return(a*b)
+
+def Divide(a,b):
+    if b==0:
+        return("MATHEMATICAL ERROR")
+    return(a/b)
+
+while True: 
+    print("""
+          1.Addition
+          2.Substraction 
+          3.Multiplication 
+          4.Division
+          5.Exit """)
+    
+    
+    
+
+    
+p=username is correct
+q=password is correct
+
+IF (p and q)
+print("login successful")
+
+Else
+print("Access Denied")
