42857701a0a6a60c04bdd269e8634ad4817e0b67
unknown <08250269.sce@rub.edu.bt>
2026-05-15 12:00:41 +0530

    student name list file creation activity, karma wangdi

--- student.py
+++ student.py
@@ -1,6 +1,5 @@
 # Creating and writing into the file
 file = open("Dummy_students_namelist", "w")
-
 file.write("STUDENT NAME OF BEd ICT I\n")
 file.write("------------------------------\n")
 file.write("karma wangdi,08250269\n") 
@@ -8,7 +7,6 @@ file.write("jetsun pema,08240342\n")
 file.write("dorji tshering,08250266\n")
 file.write("sonam tobgay,08250273\n")
 file.write("arjun,08250290\n")
-
 file.close()
 
 print("------------------------------")
@@ -16,21 +14,15 @@ print("File created successfully")
 print("------------------------------")
 
 # Reading the file
-
 file = open("Dummy_students_namelist", "r")
-
 data = file.readlines()
-
 file.close()
 
 # Asking user to enter student name
-
 name = input("Enter student name: ")
-
 found = False
 
 # Checking whether the student exists
-
 for line in data:
 
     if name in line:
